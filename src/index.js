562d17a8dfdb445e706ff4793782ea6c0dd798e2
taka <taka521@users.noreply.github.com>
2018-11-14 22:34:56 +0900

    H20 Component 1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
 import React from "react";
 import { render } from "react-dom";
 
-class Human extends React.Component {
+class H2O extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "Test" };
+    this.state = { temp: 10 };
   }
 
   render() {
     return (
-      <h2 onClick={this.onNameClick}>
-        {this.state.name} {this.props.age}
-      </h2>
+      <div>
+        <h2>{this.state.temp}</h2>
+        <button onClick={this.onPlusClick}>+</button>
+        <button onClick={this.onPlus10Click}>+10</button>
+        <button onClick={this.onMinusClick}>-</button>
+        <button onClick={this.onMinus10Click}>-10</button>
+      </div>
     );
   }
 
-  onNameClick = () => {
-    this.setState({ name: this.state.name + "san" });
+  onPlusClick = () => {
+    this.setState({ temp: this.state.temp + 1 });
+  };
+
+  onPlus10Click = () => {
+    this.setState({ temp: this.state.temp + 10 });
+  };
+
+  onMinusClick = () => {
+    this.setState({ temp: this.state.temp - 1 });
+  };
+
+  onMinus10Click = () => {
+    this.setState({ temp: this.state.temp - 10 });
   };
 }
 
-render(<Human age="3" />, document.getElementById("root"));
+render(<H2O />, document.getElementById("root"));
